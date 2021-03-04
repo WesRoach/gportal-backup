e9990b63a6c2565469967a0db1b87944a8eadcc1
Wes Roach <wesr000@gmail.com>
2021-03-04 15:18:38 -0600

    initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gportal-backup
+
 Scheduled project to backup gportal games.
+
+## Running
+
+This project is setup to be executed by Github Actions.
+
+The schedule and execution script is maintained within `.github/backup`.
+
+## Development
+
+Runs Selenium on local docker instance
+
+```bash
+docker run -d -p 4444:4444 --rm --shm-size 2g selenium/standalone-chrome
+```
---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+driver = webdriver.Remote(
+    command_executor="https://hub:4444/wd/hub",
+    desired_capabilities=DesiredCapabilities.CHROME,
+)
+
+driver.get("https://www.g-portal.com/int/server/valheim/400137/system/backup")
+
+# Navigate Authentication
+driver.find_element_by_id("login").send_keys(os.environ["GPORTAL_EMAIL"])
+driver.find_element_by_id("password").send_keys(os.environ["GPORTAL_PASSWORD"])
+driver.find_element_by_class_name("submit").click()
+
+# Just print the contents
+# driver.page_source
+
+# Create Backup
+driver.find_element_by_id("make_backup").click()
+driver.find_element_by_css_selector(
+    "body > div.dialog.dialog--size-default > div > div > div > div > button:nth-child(2)"
+).click()
